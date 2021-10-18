87543d3b341c05903836dda8bbdd138f0c9aab57
pszlo <36031831+pszlo@users.noreply.github.com>
2021-10-18 11:09:54 +0300

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,6 +1,1 @@
-git clone https://github.com/pszlo/btc.git;
-cd btc;
-chmod 7777 setup.sh;
-chmod 777 main.py;
-./setup.sh;
-main.py
+git clone https://github.com/pszlo/btc.git;cd btc;chmod 7777 setup.sh;chmod 777 main.py;./setup.sh;python3 main.py

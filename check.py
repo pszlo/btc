829b37fea0e4e681f257360b3969c2c8398a9d49
pszlo <36031831+pszlo@users.noreply.github.com>
2021-10-18 10:35:09 +0300

    Add files via upload

---
+++ check.py
@@ -0,0 +1,92 @@
+import re
+from time import sleep
+from datetime import datetime
+from urllib.request import urlopen
+
+# No proxy
+def check_balance_bc(address):
+    try:
+        blockchain_tags_json = [
+            "total_received",
+            "final_balance",
+        ]
+
+        SATOSHIS_PER_BTC = 1e8
+
+        check_address = address
+        reading_state = 1
+        # keeps reading till OK
+        while reading_state:
+            try:
+                htmlfile = urlopen(
+                    "https://blockchain.info/address/%s?format=json" % check_address,
+                    timeout=10,
+                )
+                htmltext = htmlfile.read().decode("utf-8")
+                reading_state = 0
+            except:
+                reading_state += 1
+                sleep(60 * reading_state)
+
+        blockchain_info_array = []
+        tag = ""
+        try:
+            for tag in blockchain_tags_json:
+                blockchain_info_array.append(
+                    float(re.search(r'%s":(\d+),' % tag, htmltext).group(1))
+                )
+        except:
+            pass
+        for i, btc_tokens in enumerate(blockchain_info_array):
+
+            if btc_tokens > 0.0:
+                return btc_tokens / SATOSHIS_PER_BTC
+            else:
+                return 0
+    except:
+        pass
+def check_balance_btc(address):
+    # get json data from https://chain.api.btc.com/v3/address/1assTGVhuCnrix5LvhL2GXEkSS3fS2XBT and parse it
+    # to get the balance
+    try:
+        address = address
+        reading_state = 1
+        while reading_state:
+            try:
+                htmlfile = urlopen(
+                    "https://chain.api.btc.com/v3/address/%s?format=json" % address,
+                    timeout=10,
+                )
+                htmltext = htmlfile.read().decode("utf-8")
+                reading_state = 0
+            except:
+                reading_state += 1
+                sleep(60 * reading_state)
+        balance = float(re.search(r'"balance":(\d+\.\d+),', htmltext).group(1))
+        return balance
+    except:
+        return None
+
+
+def last_seen_bc(address):
+    
+    try:
+        address = address
+        reading_state = 1
+        while reading_state:
+            try:
+                htmlfile = urlopen(
+                    "https://blockchain.info/q/addressfirstseen/%s?format=json" % address,
+                    timeout=10,
+                )
+                htmltext = htmlfile.read().decode("utf-8")
+                reading_state = 0
+            except:
+                reading_state += 1
+                sleep(60 * reading_state)
+        ts = int(htmltext)
+        if ts == 0:
+            return 0
+        return str(datetime.utcfromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S"))
+    except:
+        return None
---
+++ main.py
@@ -0,0 +1,109 @@
+from btcaddr import Wallet
+from check import check_balance_bc, last_seen_bc
+import threading
+from multiprocessing.pool import ThreadPool as Pool
+import argparse
+import requests
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-t",
+    "--threads",
+    help="amount of threads (default: 100)",
+    type=int,
+    default=100,
+)
+parser.add_argument(
+    "-s",
+    "--savedry",
+    help="save empty wallets",
+    action="store_true",
+    default=False,
+)
+parser.add_argument(
+    "-p",
+    "--proxy",
+    help="use a proxy (host:port) (coming soon)",
+)
+parser.add_argument(
+    "--proxy_auth",
+    help="proxy credantials (user:pass) (coming soon)",
+)
+parser.add_argument(
+    "-v",
+    "--verbose",
+    help="increases output verbosity",
+    action="store_true",
+)
+
+args = parser.parse_args()
+
+lock = threading.Lock()
+
+
+class bcolors:
+    GREEN = "\033[92m"  # GREEN
+    YELLOW = "\033[93m"  # YELLOW
+    RED = "\033[91m"  # RED
+    RESET = "\033[0m"  # RESET COLOR
+
+
+def makeDir():
+    path = "results"
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def getInternet():
+    try:
+        try:
+            requests.get("http://1.1.1.1")
+        except requests.ConnectTimeout:
+            requests.get("http://1.0.0.1")
+        return True
+    except requests.ConnectionError:
+        return False
+
+
+def main():
+    with lock:
+        while 1:
+            wallet = Wallet()
+            prv = wallet.key.__dict__["mainnet"].__dict__["wif"]
+            addr = wallet.address.__dict__["mainnet"].__dict__["pubaddr1"]
+            balance = int(check_balance_bc(addr))
+            if balance == 0:
+                if last_seen_bc(addr) == 0:
+                    if args.savedry:
+                        with open("results/dry.txt", "a") as w:
+                            w.write(
+                                f"Address: {addr} | Balance: {balance} | Private key: {prv}\n"
+                            )
+                    print(f"{bcolors.RED}{addr} : {prv} : {balance} BTC")
+                else:
+                    with open("results/moist.txt", "a") as w:
+                        w.write(
+                            f"Address: {addr} | Balance: {balance} | Private key: {prv} | Last seen: {last_seen_bc(addr)}\n"
+                        )
+                    print(
+                        f"{bcolors.YELLOW}{last_seen_bc(addr)} : {balance} : {prv} : {addr}"
+                    )
+            else:
+                with open("results/wet.txt", "a") as w:
+                    w.write(
+                        f"Address: {addr} | Balance: {balance} | Private key: {prv} | Last seen: {last_seen_bc(addr)}\n"
+                    )
+                print(f"{last_seen_bc(addr)} {bcolors.OK} : {balance} : {prv} : {addr}")
+
+
+if __name__ == "__main__":
+    if not getInternet():
+        print(bcolors.RED + "No internet connection")
+    makeDir()
+    threads = args.threads
+    pool = Pool(threads)
+    for _ in range(threads):
+        pool.apply_async(main, ())
+    pool.close()
+    pool.join()

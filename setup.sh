5d8aacdfbd0b1beaa12b6d96769e41cee710d38e
pszlo <36031831+pszlo@users.noreply.github.com>
2021-10-18 10:49:19 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+apt-get install python3;
+apt-get install pip;
+pip install base58;
+pip install certifi;
+pip install chardet;
+pip install ecdsa;
+pip install idna;
+pip instal requests;
+pip instal six;
+pip instal urllib3;
+
+

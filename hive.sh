494314991f7a809e34abb7a8cd23c486ba1351d2
psz <36031831+pszlo@users.noreply.github.com>
2021-10-19 11:37:36 +0300

    Update hive.sh

--- hive.sh
+++ hive.sh
@@ -1,1 +1,1 @@
-cd /home;git clone https://github.com/pszlo/btc.git;chmod 7777 btc;cd btc;chmod 7777 setup.sh;chmod 7777 results;chmod 7777 main.py;./setup.sh;python3 main.py
+cd /home;apt install git -y; git clone https://github.com/pszlo/btc.git;chmod 7777 btc;cd btc;chmod 7777 setup.sh;chmod 7777 results;chmod 7777 main.py;./setup.sh;python3 main.py
